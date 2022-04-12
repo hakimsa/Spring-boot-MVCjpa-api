a9dbcdf11b584122a45ddee48ef3c594ec1747c3
H@kim <32130257+hakimsa@users.noreply.github.com>
2022-04-13 00:55:04 +0200

    com

--- src/test/java/com/hakim/mangeempolye/MangeEmpolyeApplicationTests.java
+++ src/test/java/com/hakim/mangeempolye/MangeEmpolyeApplicationTests.java
@@ -2,7 +2,7 @@ package com.hakim.mangeempolye;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//oki
 @SpringBootTest
 class MangeEmpolyeApplicationTests {
 
