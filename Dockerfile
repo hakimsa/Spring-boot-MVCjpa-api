274caa1c6a9638d542cefe03bd732b7f1cd39590
Kim <samouh591@gmail.com>
2022-04-29 02:30:39 +0200

    add files

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
+
+  
 FROM  maven:3.5-alpine as builder
 
 COPY  ./  /app
--- src/main/java/com/hakim/mangeempolye/MangeEmpolyeApplication.java
+++ src/main/java/com/hakim/mangeempolye/MangeEmpolyeApplication.java
@@ -8,7 +8,6 @@ import com.hakim.mangeempolye.services.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MangeEmpolyeApplication {
