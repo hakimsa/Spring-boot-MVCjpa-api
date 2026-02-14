63b7d8ebacfff4592085418f036648e43282a7a8
hakimsa <samouh591@gmail.com>
2026-02-15 00:33:09 +0100

    test concept

--- src/main/java/com/hakim/mangeempolye/config/SecurityConfig.java
+++ src/main/java/com/hakim/mangeempolye/config/SecurityConfig.java
@@ -14,16 +14,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .cors().and()  // Asegúrate de que CORS esté habilitado
-            .csrf().disable()  // Desactiva CSRF temporalmente para pruebas
-            .authorizeRequests()
-            .antMatchers("/employee/api/v1/add").permitAll() 
-             .antMatchers("/employee/api/v1/find ").permitAll() 
-             .antMatchers("/actuator/health").permitAll()
-            .anyRequest().authenticated();
- // Permite acceso sin autenticación
-         //   .antMatchers("/employee/api/v1/all").permitAll() 
-           // Permite acceso sin autenticación
-          //  .anyRequest().authenticated();  // Requiere autenticación para otros endpoints
+            
+        .cors().and()
+        .csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/employee/api/v1/add").permitAll()
+        .antMatchers("/employee/api/v1/find").permitAll()
+        .antMatchers("/employee/api/v1/all").permitAll()  // <--- agregado
+        .antMatchers("/actuator/health").permitAll()
+        .anyRequest().authenticated();
     }
 }
